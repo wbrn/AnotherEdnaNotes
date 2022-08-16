82ea36e55c963d3f4b9c09a8304e7c064dba92f7
wbrn <111114827+wbrn@users.noreply.github.com>
2022-08-16 17:28:45 +0300

    Initial commit

---
+++ app/src/main/java/ru/dsavelev/anotherednanotes/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package ru.dsavelev.anotherednanotes.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
